f9cf1cd8da906848977c92e5b6dca083aad6ba47
liangtianyu <liangtianyu@uniondrug.cn>
2022-02-24 09:23:25 +0800

    药品批量上下架

---
+++ shangou/medicine_data.go
@@ -0,0 +1,6 @@
+package shangou
+
+type MedicineData struct {
+	AppMedicineCode string `json:"app_medicine_code"` //是否必须：是；描述：APP方药品id，即商家中台系统里药品的编码
+	IsSoldOut       int    `json:"is_sold_out"`       //是否必须：是；描述：药品上下架状态；取值范围：0上架，1下架
+}
