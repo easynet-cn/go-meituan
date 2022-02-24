9e3340c3bfdb679afe8b83518e1391b75b1c2b40
liangtianyu <liangtianyu@uniondrug.cn>
2022-02-24 16:08:52 +0800

    【折扣】查询门店药品折扣商品和爆品商品活动

--- shangou/act_full_discount_shippingfee_list_response_data_item.go
+++ shangou/act_full_discount_shippingfee_list_response_data_item.go
@@ -5,7 +5,7 @@ type ActFullDiscountShippingfeeListResponseDataItem struct {
 	ActId      int64    `json:"act_id"`       //描述：活动id
 	StartTime  int      `json:"start_time"`   //描述：活动开始时间，传10位秒级的时间戳。系统已自动将传入的时间转换为所传日期的00:00:00。
 	EndTime    int      `json:"end_time"`     //描述：活动结束时间，传10位秒级的时间戳。系统已自动将传入的时间转换为所传日期的23:59:59。
-	WeeksTime  string   `json:"end_time"`     //描述：生效活动周期，1～7表示周一至周日，多个星期以英文逗号分隔。如创建活动时此字段未填写，则默认周期为"1,2,3,4,5,6,7"。
+	WeeksTime  string   `json:"weeks_time"`   //描述：生效活动周期，1～7表示周一至周日，多个星期以英文逗号分隔。如创建活动时此字段未填写，则默认周期为"1,2,3,4,5,6,7"。
 	Period     string   `json:"period"`       //描述：生效时间段，多个时段以英文逗号分隔；如创建活动时此字段未填写，则默认时段为"00:00:23:59"。
 	ActDetail  string   `json:"act_detail"`   //描述：阶梯满减活动详情，json格式数组。
 	MaxPrice   *float64 `json:"max_price"`    //描述：免配送费门槛，商品消费大于或等于该金额时免除全部配送费。若在创建时此字段未设置，则查询时值为null。
---
+++ shangou/act_retail_discount_list_request.go
@@ -0,0 +1,12 @@
+package shangou
+
+//【折扣】查询门店药品折扣商品和爆品商品活动
+//请求方式：GET
+//请求地址：https://waimaiopen.meituan.com/api/v1/act/retail/discount/list
+//文档地址：https://open-shangou.meituan.com/home/docDetail/306
+type ActRetailDiscountListRequest struct {
+	AppPoiCode string `url:"app_poi_code"` //是否必须：是；描述：APP方门店id，即商家中台系统里门店的编码。如商家在操作绑定门店至开放平台应用中时，未绑定三方门店id信息，则默认APP方门店id与美团门店id相同。
+	ActType    int    `url:"act_type"`     //是否必须：否；描述：活动类型可取值： 1001折扣活动 56爆品活动 如不传本参数，则默认为查询折扣活动。
+	Offset     int    `url:"offset"`       //是否必须：是；描述：分页查询偏移量，表示从第几条数据开始查，0表示第一条。
+	Limit      int    `url:"limit"`        //是否必须：是；描述：分页查询每页展示条数，此字段信息最大不超过200。
+}
