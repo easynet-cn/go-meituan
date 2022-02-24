12d1fbe33a3d65b5f5738c9582c5bf6fc5694bca
liangtianyu <liangtianyu@uniondrug.cn>
2022-02-24 16:20:44 +0800

    【折扣】批量删除药品折扣商品和和爆品商品活动

---
+++ shangou/act_retail_discount_batchdelete_response.go
@@ -0,0 +1,6 @@
+package shangou
+
+type ActRetailDiscountBatchdeleteResponse struct {
+	Data      string                                        `json:"data"`
+	ExtraInfo ActRetailDiscountBatchdeleteResponseExtraInfo `json:"extra_info"`
+}
