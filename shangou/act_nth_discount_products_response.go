252137de52fd830e627ee293b6c93abb5f79e7f2
liangtianyu <liangtianyu@uniondrug.cn>
2022-03-01 10:49:59 +0800

    【第N件优惠】查询第N件优惠活动中的商品信息

---
+++ shangou/act_nth_discount_products_response.go
@@ -0,0 +1,6 @@
+package shangou
+
+type ActNthDiscountProductsResponse struct {
+	Data  string `json:"data"`  //描述：查询成功，则为活动商品信息集合 查询失败，则为 ng
+	Error string `json:"error"` //描述：失败信息
+}
---
+++ shangou/act_nth_discount_products_response_data_item.go
@@ -0,0 +1,6 @@
+package shangou
+
+type ActNthDiscountProductsResponseDataItem struct {
+	AppFoodCode string `json:"app_food_code"`
+	DayLimit    int    `json:"day_limit"`
+}
