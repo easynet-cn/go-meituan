1fcf0662d417d755966ee7d21156b70d18a356b0
liangtianyu <liangtianyu@uniondrug.cn>
2022-03-08 13:03:16 +0800

    查询门店经营品类列表

---
+++ poi/ecommerce_poi_settle_category_list_request.go
@@ -0,0 +1,8 @@
+package poi
+
+//查询门店经营品类列表
+//请求方式：GET
+//请求地址：https://waimaiopen.meituan.com/api/v1/ecommerce/poi/settle/category/list
+//文档地址：https://open-shangou.meituan.com/home/docDetail/534
+type EcommercePoiSettleCategoryListRequest struct {
+}
