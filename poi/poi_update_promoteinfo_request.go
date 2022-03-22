b792fe3616681a7c81c03b751d149ea1a009bd07
liangtianyu <liangtianyu@uniondrug.cn>
2022-03-22 14:30:16 +0800

    修改更改门店公告信息请求url错误定义

--- poi/poi_update_promoteinfo_request.go
+++ poi/poi_update_promoteinfo_request.go
@@ -5,6 +5,6 @@ package poi
 //请求地址：https://waimaiopen.meituan.com/api/v1/poi/updatepromoteinfo
 //文档地址：https://open-shangou.meituan.com/home/docDetail/27
 type PoiUpdatepromoteinfoRequest struct {
-	AppPoiCode    string `url:"app_poi_codes"`  //是否必须：是；描述：APP方门店id，传商家中台系统里门店的编码。如商家在操作绑定门店至开放平台应用中时，未绑定三方门店id信息，则默认APP方门店id与美团门店id相同。
+	AppPoiCode    string `url:"app_poi_code"`   //是否必须：是；描述：APP方门店id，传商家中台系统里门店的编码。如商家在操作绑定门店至开放平台应用中时，未绑定三方门店id信息，则默认APP方门店id与美团门店id相同。
 	PromotionInfo string `url:"promotion_info"` //是否必须：是；描述：门店公告信息
 }
